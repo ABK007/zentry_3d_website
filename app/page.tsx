42e815bee2b3e3e32615e86bbf43923908b1c1a2
Muhammad Abubakar Khalid <abubakarzzz07@gmail.com>
2024-11-23 10:31:14 -0800

    updated the global.css file

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-5xl text-violet-300 font-bold font-circular-web ">
-        Welcome to Awwwaaards
-      </h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   );
 }
