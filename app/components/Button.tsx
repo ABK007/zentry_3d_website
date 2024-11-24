0efc59083d04277ed7000813630d84d371b9f6c8
Muhammad Abubakar Khalid <abubakarzzz07@gmail.com>
2024-11-23 21:53:15 -0800

    animated hero section

---
+++ app/components/Button.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ButtonProps } from "../types";
+
+const Button = ({
+  id,
+  title,
+  leftIcon,
+  containerClass,
+  rightIcon,
+}: ButtonProps) => {
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full 
+        bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
+
+      <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+      {rightIcon}
+    </button>
+  );
+};
+
+export default Button;
---
+++ app/types/index.ts
@@ -0,0 +1,7 @@
+export interface ButtonProps {
+  id: string;
+  title: string;
+  leftIcon: React.ReactNode;
+  containerClass: string;
+  rightIcon?: React.ReactNode;
+}
